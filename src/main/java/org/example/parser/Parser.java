d3374875ce8f346ae4956ba3c22e1f4f8ad742c7
nkozlov <kozlovatatiana@list.ru>
2023-09-27 12:23:33 +0600

    parser quiz

---
+++ src/main/java/org/example/parser/Parser.java
@@ -0,0 +1,55 @@
+package org.example.parser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * This class is thread safe.
+ */
+public class Parser {
+
+    private File file;
+
+    public synchronized File getFile() {
+        return file;
+    }
+
+    public synchronized void setFile(File f) {
+        file = f;
+    }
+
+    public String getContent() throws IOException {
+        FileInputStream i = new FileInputStream(file);
+        String output = "";
+        int data;
+        while ((data = i.read()) > 0) {
+            output += (char) data;
+        }
+        return output;
+    }
+
+    public String getContentWithoutUnicode() throws IOException {
+        FileInputStream i = new FileInputStream(file);
+        String output = "";
+        int data;
+        while ((data = i.read()) > 0) {
+            if (data < 0x80) {
+                output += (char) data;
+            }
+        }
+        return output;
+    }
+
+    public void saveContent(String content) {
+        try {
+            FileOutputStream o = new FileOutputStream(file);
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
